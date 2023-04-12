d79ee1ce9b05ee49057edfb4e39c4f4d8e2d6bb0
Jacob Rubino <jacobrubino@gmail.com>
2023-04-12 16:09:08 -0400

    generates a readme file but needs a bit of optimization

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
-// const questions = [
-// ];
+
 const questions = [
     {
       type: "input",
@@ -32,18 +30,18 @@ const questions = [
     },
     {
       type: "list",
-      name: "Liscense",
-      message: "What liscense are you using?",
+      name: "license",
+      message: "What license are you using?",
       choices: [
         "None",
         "MIT",
-        "Mozilla Public License 2.0",
-        "GNU Affero General Public License v3.0",
+        "Mozilla-Public-License-2.0",
+        "GNU-Affero-General-Public-License-v3.0",
       ],
     },
     {
       type: "list",
-      name: "LiscBadge",
+      name: "licBadge",
       message: "What Badge Design will you use ",
       choices: [
         "Green",
@@ -54,51 +52,37 @@ const questions = [
     },
 ];
 
-const templateReadMe = 
-`
-# ${Projname}
-
-## ${Projname} Description
-
-${ProjFunc}
-
-##  ${Projname} Installation requirements
-\`\`\`bash
-${ProjInstall}
-\`\`\`
-
-## ${Projname} Usage
-
-${projExample}
-
-## ${Projname} Contributors
-
-${ProjContributors}
-
-## Liscense
-[${LiscBadge}](liscense)
-`;
-
-
-// the question that will read the questions will loop through the question array, if a "is there more" question is answered
-// in a way that means it needs another input (for a list item) make the argv value go up, and the index go down.
-// Loop by question array length so if we repeat a question the index will go down, but have a count that goes up for each time the loop is called for a higher argv value each time.
-// TODO: Create a function to write README file
-
-fs.writeFile("README.md", templateReadMe, (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
-
-// TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions)
   .then((answers) =>   {
-    const {ProjName, ProjFunc, ProjInstall, ProjExample, ProjContributors, Liscense, LiscBadge} = answers
-    const filledTemplate = ({ProjName, ProjFunc, ProjInstall, ProjExample, ProjContributors, Liscense, LiscBadge}) => {templateReadMe}
+    const {ProjName, ProjFunc, ProjInstall, ProjExample, ProjContributors, license, licBadge} = answers
+    const templateReadMe = 
+    `# ${ProjName}
     
-
+    ## ${ProjName} Description
+    
+    ${ProjFunc}
+    
+    ##  ${ProjName} Installation requirements
+    \`\`\`bash
+    ${ProjInstall}
+    \`\`\`
+    
+    ## ${ProjName} Usage
+    
+    ${ProjExample}
+    
+    ## ${ProjName} Contributors
+    
+    ${ProjContributors}
+    
+    ## license
+    [![License: ${license}](https://img.shields.io/badge/License-${license}-${licBadge}.svg)](https://opensource.org/licenses/${license})
+    `;
+    fs.writeFile('README.MD', templateReadMe, function (err) {
+      if (err) throw err;
+      console.log('IT WORKED')
+    })
   })
 }
 
