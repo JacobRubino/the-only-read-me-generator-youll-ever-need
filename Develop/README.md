76f8a2cf838fc40789f0cb1ab5b8deee810277a5
Jacob Rubino <jacobrubino@gmail.com>
2023-07-03 18:56:33 -0400

    minor adjustment to tests

---
+++ Develop/README.md
@@ -0,0 +1,49 @@
+
+# ReadMe generator
+
+This will generate a readme based off of the users input.
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+
+<a name="description"></a>
+## Description
+This will generate a readme based off of the users input.
+
+<a name="installation"></a>
+## Installation
+Node.js and inquirer
+
+<a name="usage"></a>
+## Usage
+Run index.js through node, answer the prompted questions.
+
+<a name="license"></a>
+## License
+This application is covered under the None license.
+
+[![License: None](https://img.shields.io/badge/License-None-blueviolet.svg)](https://opensource.org/licenses/None)
+
+<a name="contributing"></a>
+## Contributing
+Mozilla webdocs, stack overflow. 
+
+<a name="tests"></a>
+## Tests
+Please run the following command to run tests:
+```bash
+npm test
+```
+
+<a name="questions"></a>
+## Questions
+If you have any questions, please feel free to reach out to me via email or through my GitHub profile.
+
+Email: jacobrubino@gmail.com
+GitHub: [github.com/jacobrubino](https://github.com/jacobrubino)
--- Develop/index.js
+++ Develop/index.js
@@ -1,110 +1,132 @@
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 const questions = [
-    {
-      type: "input",
-      name: "ProjName",
-      message: "What is your project name?",
-    },
-    {
-      type: "input",
-      name: "ProjFunc",
-      message: "What does your project do?",
-    },
-    {
-      type: "input",
-      name: "ProjInstall",
-      message: "what is required for installation?",
-    },
-    {
-      type: "input",
-      name: "ProjExample",
-      message: "How do you install your program?",
-    },
-    {
-      type: "input",
-      name: "ProjContributors",
-      message: "are there any major contributors you would like to reference?",
-    }, 
-    {
-      type: "text",
-      name: "GHuser",
-      message: "What is your github username?",
-    },
-    {
-      type: "text",
-      name: "UserEmail",
-      message: "Please enter your email.",
-    }, 
-    {
-      type: "list",
-      name: "license",
-      message: "What license are you using?",
-      choices: [
-        "None",
-        "MIT",
-        "Mozilla-Public-License-2.0",
-        "GNU-Affero-General-Public-License-v3.0",
-      ],
-    },
-    {
-      type: "list",
-      name: "licBadge",
-      message: "What Badge Design will you use ",
-      choices: [
-        "brightgreen",
-        "blue",
-        "red",
-        "blueviolet",
-      ],
-    },
+  {
+    type: "input",
+    name: "ProjName",
+    message: "What is your project name?",
+  },
+  {
+    type: "input",
+    name: "ProjFunc",
+    message: "What does your project do?",
+  },
+  {
+    type: "input",
+    name: "ProjInstall",
+    message: "What is required for installation?",
+  },
+  {
+    type: "input",
+    name: "ProjExample",
+    message: "How do you use your program?",
+  },
+  {
+    type: "input",
+    name: "ProjContributors",
+    message: "Are there any major contributors you would like to reference?",
+  },
+  {
+    type: "text",
+    name: "GHuser",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "ProjTest",
+    message: "Please provide instructions for running tests:",
+  },
+  {
+    type: "text",
+    name: "UserEmail",
+    message: "Please enter your email.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license are you using?",
+    choices: [
+      "None",
+      "MIT",
+      "Mozilla-Public-License-2.0",
+      "GNU-Affero-General-Public-License-v3.0",
+    ],
+  },
+  {
+    type: "list",
+    name: "licBadge",
+    message: "What badge design will you use?",
+    choices: [
+      "brightgreen",
+      "blue",
+      "red",
+      "blueviolet",
+    ],
+  },
 ];
+
+function generateReadme(answers) {
+  const { ProjName, ProjFunc, ProjInstall, ProjExample, ProjContributors, license, licBadge, UserEmail, GHuser } = answers;
+  const templateReadMe = `
+# ${ProjName}
+
+${ProjFunc}
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+
+<a name="description"></a>
+## Description
+${ProjFunc}
+
+<a name="installation"></a>
+## Installation
+${ProjInstall}
+
+<a name="usage"></a>
+## Usage
+${ProjExample}
+
+<a name="license"></a>
+## License
+This application is covered under the ${license} license.
+
+[![License: ${license}](https://img.shields.io/badge/License-${license.replace(/-/g, '--')}-${licBadge}.svg)](https://opensource.org/licenses/${license})
+
+<a name="contributing"></a>
+## Contributing
+${ProjContributors}
+
+<a name="tests"></a>
+## Tests
+${ProjTest}
+
+<a name="questions"></a>
+## Questions
+If you have any questions, please feel free to reach out to me via email or through my GitHub profile.
+
+Email: ${UserEmail}
+GitHub: [github.com/${GHuser}](https://github.com/${GHuser})
+`;
+
+  fs.writeFile('README.md', templateReadMe, function (err) {
+    if (err) throw err;
+    console.log('README.md file has been generated successfully.');
+  });
+}
 
 function init() {
   inquirer.prompt(questions)
-  .then((answers) =>   {
-    const {ProjName, ProjFunc, ProjInstall, ProjExample, ProjContributors, license, licBadge, UserEmail, GHuser} = answers
-    const templateReadMe = 
-`# ${ProjName}
-    
-${ProjFunc}
-
-1. [Installation requirements](#inst)
-2. [Useage](#usage)
-3. [Contributors](#contrib)
-4. [Contact and github](#contact)
-<a name="inst"></a>
-## ${ProjName} Installation requirements
-
-\`\`\`bash
-${ProjInstall}
-\`\`\`
-
-<a name="usage"></a>
-## ${ProjName} Usage
-    
-${ProjExample}
-<a name="contrib"></a>
-## ${ProjName} Contributors
-    
-${ProjContributors}
-
-<a name="contact"></a>
-## Questions
-If you have any questions please reach me through my email at ${UserEmail}.
-Check out my github! [github.com/${GHuser}](github.com/${GHuser})
-    
-## license
-[![License: ${license}](https://img.shields.io/badge/License-${license.replace(/-/g, '--')}-${licBadge}.svg)](https://opensource.org/licenses/${license})
-`;
-    fs.writeFile('README.MD', templateReadMe, function (err) {
-      if (err) throw err;
-      console.log('IT WORKED')
-    })
-  })
-}
-
-// Function call to initialize app
+    .then(generateReadme)
+    .catch((err) => {
+      console.error(err);
+    });
+};
 init();
